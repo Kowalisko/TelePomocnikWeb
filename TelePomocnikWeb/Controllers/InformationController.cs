f0e39ecf2d0b99379fd9ce8a3890666a9ef1f028
Rafał <rafa.kowalewsk@gmail.com>
2015-06-11 20:07:43 +0200

    Initial commit

---
+++ TelePomocnikWeb/Controllers/InformationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelePomocnikWeb.Controllers
+{
+    public class InformationController : Controller
+    {
+        public JsonResult GetInformations()
+        {
+            TelePomocnikDbEntities1 db = new TelePomocnikDbEntities1();
+            var informations = db.MustCallInformation.ToList();
+            return Json(informations, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
